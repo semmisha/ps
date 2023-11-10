6ac6a00c2c51d68f620efdceeba6b19f5d815893
semmisha <101043299+semmisha@users.noreply.github.com>
2023-11-10 21:29:23 +0300

    Add files via upload

---
+++ MMF_inst.ps1
@@ -0,0 +1,12 @@
+﻿#Enable script to run
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+#Uninstall Office365
+winget uninstall "O365ProPlusRetail - ru-ru"
+#Uninstall MS.Teams
+winget uninstall "Microsoft.Teams"
+#Set password variable
+$password = ConvertTo-SecureString "Visitor@6" -AsPlainText -Force
+#Create Local Admin
+New-LocalUser -Name 'MMFadmin' -Description 'MMF admin - DO NOT TOUCH!!!' -AccountNeverExpires -UserMayNotChangePassword -Password $password -PasswordNeverExpires
+#Add MMFadmin to Admin Group
+Add-LocalGroupMember -Group "Administrators" -Member "MMFadmin"
